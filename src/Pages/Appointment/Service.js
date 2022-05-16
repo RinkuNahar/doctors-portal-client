678e5371c91492f41e9c2a7568898ead81646d91
RinkuNahar <rinkunaharhossan2022@gmail.com>
2022-05-16 12:19:29 +0600

    firebase initialized

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -5,16 +5,16 @@ const Service = ({ service, setTreatment }) => {
     const { name, slots } = service;
 
     return (
-        <div class="card lg:max-w-lg bg-base-100 shadow-xl mx-11">
-            <div class="card-body">
-                <h2 class="card-title text-secondary">{name}</h2>
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl mx-11">
+            <div className="card-body">
+                <h2 className="card-title text-secondary">{name}</h2>
                 <p>{
                     slots.length > 0 ? <span>{slots[0]}</span> : <span className='text-red-600'>No Space Available</span>
                 }
                 </p>
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'}Available</p>
-                <div class="card-actions mx-auto">
-                    <label disabled={slots.length === 0} onClick={() => setTreatment(service)} for="booking-modal" class="btn modal-button btn-secondary text-white">Book Appointment</label>
+                <div className="card-actions mx-auto">
+                    <label disabled={slots.length === 0} onClick={() => setTreatment(service)} htmlFor="booking-modal" className="btn modal-button btn-secondary text-white">Book Appointment</label>
                 </div>
             </div>
         </div>
